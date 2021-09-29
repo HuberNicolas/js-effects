4117a4fc6b56fe33ffadd2d6c900bf054212a5f0
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2021-09-29 11:07:13 +0200

    Simple Triangles

---
+++ particles/script4.js
@@ -0,0 +1,76 @@
+// blazing particles
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const linesArray = [];
+
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+class Line {
+    constructor() {
+        this.x = Math.random() * canvas.height;
+        this.y = Math.random() * canvas.height;
+        this.length = 100;
+        this.color = [64, Math.random() * 64, Math.random() * 64];
+    }
+
+    update() {
+
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = `rgb(
+            0,
+            ${Math.floor(255 - 42.5 * 10*Math.random())},
+            ${Math.floor(255 - 42.5 * 1*Math.random())})`;
+        ctx.lineWidth = 5;
+        
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(this.x + this.length, this.y + this.length);
+        ctx.stroke();
+
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(this.x - this.length, this.y + this.length);
+        ctx.stroke();
+
+        ctx.moveTo(this.x - this.length, this.y + this.length)
+        ctx.lineTo(this.x + this.length, this.y + this.length);
+        ctx.stroke();
+    }
+}
+
+
+
+
+function init() {
+    for (let i = 0; i < 2; i++) {
+        linesArray.push(new Line());
+    }
+}
+
+
+function handleParticles() {
+    for (let i = 0; i < linesArray.length; i++) {
+        linesArray[i].update();
+        linesArray[i].draw();
+    }
+}
+
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+
+
+init();
+animate();
